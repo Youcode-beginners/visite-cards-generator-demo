6818023ed5a6f36f58a01124647e5ccf11e6491d
zakaria_batty <zbatty1297@gmail.com>
2020-01-19 13:38:24 +0100

    finally

---
+++ assets/js/style.js
@@ -0,0 +1,25 @@
+// let number;
+// const PI = 3.14;
+
+// let num_1 = 10;
+// let num_2 = 20;
+
+// console.log(num_1 + num_2);
+
+let age = 6;
+let message = "";
+
+// console.log( age < 21 )
+
+
+// if ( age < 10 ) { message = "dor haied"; }
+// if ( age > 10 ) { message = "marhba"; }
+
+// if ( age < 10 ) { message = "dor haied" }
+// else { message = "marhba"; }
+
+if ( age < 10 ) { message = "dor haied" }
+else if ( age < 20 ) { message = "Jib wali 2amrek" }
+else { message = "marhba"; }
+
+console.log(message)
